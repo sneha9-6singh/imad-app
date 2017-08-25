725c8e612316c975d14c3fb053cf246340a9ab06
sneha9-6singh <souravmailme199@gmail.com>
2017-08-26 02:29:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/article-gh', function(req, res){
- res.sendFile('article 1 is requested and will be served');   
+ res.send('article 1 is requested and will be served');   
 });
 
 
